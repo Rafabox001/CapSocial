38be842c97dc2162d5b451f3860aa456de4c4fee
Rafabox001 <rafabox001@gmail.com>
2017-07-27 12:47:59 -0500

    login implementation

--- app/src/main/java/com/example/rafaeldomingo/testapp/LoginActivity.java
+++ app/src/main/java/com/example/rafaeldomingo/testapp/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -78,8 +79,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private UserLoginTask mAuthTask = null;
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
+    @BindView(R.id.input_layout_user)TextInputLayout layoutUser;
+    @BindView(R.id.input_layout_password)TextInputLayout layoutPass;
+    @BindView(R.id.user)EditText user;
+    @BindView(R.id.pass)EditText pass;
+    @BindView(R.id.forgot_pass)EditText recovery;
+
     private View mProgressView;
     private View mLoginFormView;
 
@@ -92,30 +97,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         //set up facebook login
         setUpFacebookLogin();
-
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
-
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
